75df23bf52fe53e8ccbacbcbd04b4b935d2301b9
Frank Zhe Wang <frank.wz@gmail.com>
2015-11-30 19:23:16 -0600

    Create output and log folders if they do not exist

--- three-store/src/run-gen-demand.sh
+++ three-store/src/run-gen-demand.sh
@@ -4,9 +4,13 @@ then
   echo "Please specify output path."
   exit 1
 fi
+
+mkdir -p $1
+mkdir -p ../log/demand
+
 while read id a m gm; do
   bsub -M8 -q hour -g /gendemand -J "demand${id}" \
     matlab -nodisplay -nosplash -singleCompThread \
-    -r "../src/job_gen_demand('${id}', $a, $m, $gm, '${1}')" \
+    -r "job_gen_demand('${id}', $a, $m, $gm, '${1}')" \
     -logfile "../log/demand/${id}.log"
 done < params-demand.txt
