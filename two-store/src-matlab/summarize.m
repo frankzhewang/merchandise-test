ded18e5dcb9a1705404955f50fec464984cda10b
Frank Zhe Wang <frank.wz@gmail.com>
2015-12-16 15:25:25 -0600

    Add summarize script for two-store

---
+++ two-store/src-matlab/summarize.m
@@ -0,0 +1,10 @@
+% Matlab script
+% Summarize expected profit under various policies for two-store problem
+
+fprintf('Summarizing profits with timing info ...\n');
+summarize_timing
+fprintf('Done.\n')
+
+fprintf('Summarizing profits without timing info ...\n');
+summarize_no_timing
+fprintf('Done.\n')
